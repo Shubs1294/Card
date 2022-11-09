a247c6a34182e03e76e235503212a86163769303
Shubs1294 <108142924+Shubs1294@users.noreply.github.com>
2022-11-09 17:20:51 +0530

    Add files via upload

---
+++ Data/CardsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Cards.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cards.API.Data
+{
+    public class CardsDbContext : DbContext
+    {
+        public CardsDbContext(DbContextOptions options): base(options)
+        {
+        }
+        //DbSet for creating tables in Database
+        public DbSet<Card> Cards { get; set; }
+    }
+}
